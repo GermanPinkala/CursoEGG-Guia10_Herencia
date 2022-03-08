5f94b0c889a487e735c54934f511124cbfd69317
gercsz <gerberto22@gmail.com>
2022-03-08 10:27:10 -0300

    ejercicio 4

--- Guia10Eje2y3/src/ejercicio02/Lavadora.java
+++ Guia10Eje2y3/src/ejercicio02/Lavadora.java
@@ -37,15 +37,9 @@ public class Lavadora extends Electrodomestico {
         this.carga = leer.nextInt();
     }
 
-    /*Método precioFinal(): este método será heredado y se le sumará la siguiente
-funcionalidad. Si tiene una carga mayor de 30 kg, aumentará el precio en $500,
-si la carga es menor o igual, no se incrementará el precio. Este método debe
-llamar al método padre y añadir el código necesario. Recuerda que las
-condiciones que hemos visto en la clase Electrodoméstico también deben
-afectar al precio.*/
     @Override
     public void precioFinal() {
-        
+
         super.precioFinal();
         if (this.carga > 30) {
             this.precio += 500;
@@ -57,7 +51,5 @@ afectar al precio.*/
     public String toString() {
         return super.toString() + "Lavadora{" + "carga=" + carga + '}';
     }
-    
-    
 
 }
---
+++ Guia10Eje4/src/ejercicio4/calculosFormas.java
@@ -0,0 +1,11 @@
+package ejercicio4;
+
+public interface calculosFormas {
+
+    Double PI = Math.PI;
+
+    Double calcularArea();
+
+    Double calcularPerimetro();
+
+}
